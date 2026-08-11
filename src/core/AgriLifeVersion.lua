9688222c18bda3eac02eec593ab544dd1527b396
squall39 <38831480+squall39@users.noreply.github.com>
2026-08-11 22:27:25 +0200

    Publish current version source

---
+++ src/core/AgriLifeVersion.lua
@@ -0,0 +1,18 @@
+-- Copyright (C) 2026 Chez_Squall. All rights reserved.
+-- AgriLife Manager original code: official source repository and approved distribution channels.
+AgriLife = AgriLife or {}
+
+AgriLife.Version = {
+    MOD = "0.7.9.0",
+    SAVE_SCHEMA = 3,
+    SETTINGS_SCHEMA = 2,
+    DISPLAY_NAME = "AgriLife Manager",
+    SAVE_FILE = "AgriLifeManager.xml",
+    MOD_NAME = g_currentModName or "FS25_AgriLifeManager",
+    SETTINGS_NAMESPACE = "FS25_AgriLifeManager",
+    MOD_DIR = g_currentModDirectory or ""
+}
+
+function AgriLife.Version.getDisplayVersion()
+    return AgriLife.Version.MOD
+end
