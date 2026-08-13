90d811617946fab7a8faa613de74dd2c7b609c6d
squall39 <38831480+squall39@users.noreply.github.com>
2026-08-13 18:32:37 +0200

    Advance runtime version to 0.9.3.18

--- src/core/AgriLifeVersion.lua
+++ src/core/AgriLifeVersion.lua
@@ -2,7 +2,7 @@
 AgriLife = AgriLife or {}
 
 AgriLife.Version = {
-    MOD = "0.9.3.17",
+    MOD = "0.9.3.18",
     SAVE_SCHEMA = 4,
     SETTINGS_SCHEMA = 2,
     DISPLAY_NAME = "AgriLife Manager",
