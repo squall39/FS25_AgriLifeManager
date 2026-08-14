67c8fe0875e913d8513ac8c16eb85b73a99ecfe5
squall39 <38831480+squall39@users.noreply.github.com>
2026-08-14 15:57:49 +0200

    Update AgriLife test version to 0.9.3.32

--- src/core/AgriLifeVersion.lua
+++ src/core/AgriLifeVersion.lua
@@ -2,7 +2,7 @@
 AgriLife = AgriLife or {}
 
 AgriLife.Version = {
-    MOD = "0.9.3.31",
+    MOD = "0.9.3.32",
     SAVE_SCHEMA = 4,
     SETTINGS_SCHEMA = 2,
     DISPLAY_NAME = "AgriLife Manager",
