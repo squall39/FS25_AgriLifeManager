8b8804a8f46642008d72fe3b43d8613aeff61dfb
squall39 <38831480+squall39@users.noreply.github.com>
2026-08-14 04:13:57 +0200

    Update runtime version to 0.9.3.26

--- src/core/AgriLifeVersion.lua
+++ src/core/AgriLifeVersion.lua
@@ -2,7 +2,7 @@
 AgriLife = AgriLife or {}
 
 AgriLife.Version = {
-    MOD = "0.9.3.25",
+    MOD = "0.9.3.26",
     SAVE_SCHEMA = 4,
     SETTINGS_SCHEMA = 2,
     DISPLAY_NAME = "AgriLife Manager",
