e65f127b10e349d0189e6f2fabdd46e6e7518b65
squall39 <38831480+squall39@users.noreply.github.com>
2026-08-14 07:17:40 +0200

    Prepare AgriLife Manager 0.9.3.27 test

--- src/core/AgriLifeVersion.lua
+++ src/core/AgriLifeVersion.lua
@@ -2,7 +2,7 @@
 AgriLife = AgriLife or {}
 
 AgriLife.Version = {
-    MOD = "0.9.3.26",
+    MOD = "0.9.3.27",
     SAVE_SCHEMA = 4,
     SETTINGS_SCHEMA = 2,
     DISPLAY_NAME = "AgriLife Manager",
